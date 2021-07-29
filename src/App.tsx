397967a01900d54e4fe06b103a4ebb9b3de120bd
Sven Vowe <svenvowe@gmail.com>
2021-07-29 13:45:47 +0200

    basic boilerplate setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
-interface AppProps {}
+const TestStyles = styled.div`
+    text-align: false;
+`;
 
-function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => {
+    // Create the count state.
+    const [count, setCount] = useState(0);
+    // Create the counter (+1 every second).
+    useEffect(() => {
+        const timer = setTimeout(() => setCount(count + 1), 1000);
+        return () => clearTimeout(timer);
+    }, [count, setCount]);
+    // Return the App component.
+    return (
+        <TestStyles className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <p>
+                    Page has been open for <code>{count}</code> seconds.
+                </p>
+                <p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </p>
+            </header>
+        </TestStyles>
+    );
+};
 
 export default App;
